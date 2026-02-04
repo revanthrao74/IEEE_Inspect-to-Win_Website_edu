03c630a3abfaf833e7598dffc7d800af963538d4
DuurgaaPrasadReddy <duurgaaprasadreddy@gmail.com>
2026-02-04 23:41:46 +0530

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/IEEE_Inspect-to-Win_Website/',
+  base: 'IEEE_Inspect-to-Win_Website',
   plugins: [react()],
 })
