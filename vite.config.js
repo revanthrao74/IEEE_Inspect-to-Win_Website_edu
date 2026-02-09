5bcb7a19d5ba8e159d499e8e8b7783c963ad1bf7
revanth <revanthrao64@gmail.com>
2026-02-09 22:39:45 +0530

    random

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/IEEE_Inspect-to-Win_Website/',
+  base: '/IEEE_Inspect-to-Win_Website_edu/',
   plugins: [react()],
 })
